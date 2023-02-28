c592bdb581a29e31a1bfc2c76e44ce00d36dc8f9
museekee <103117239+museekee@users.noreply.github.com>
2023-03-01 02:23:53 +0900

    명령어 추가

--- web/assets/scripts/land.js
+++ web/assets/scripts/land.js
@@ -5,6 +5,10 @@ const E = {
             main: document.getElementById("DevInfo"),
             posX: document.getElementById("DEV_position_x"),
             posY: document.getElementById("DEV_position_y"),
+        },
+        devConsole: {
+            main: document.getElementById("DevConsole"),
+            input: document.getElementById("DEV_chat")
         }
     }
 }
@@ -49,8 +53,18 @@ function renderByPos(x, y, data) {
     WORLD[y][x] = data
     E_WORLD[y][x].setAttribute("class", data)
 }
+function renderPlayer(oldX, oldY, x, y) {
+    E_WORLD[oldY][oldX].setAttribute("class", WORLD[oldY][oldX]) // 플레이어가 있던 곳을 원래 데이터로 변경
+    Player[0] = x
+    Player[1] = y
+    GameElemTranslate[0] = 20 * (xCount / 2) - (20 * x)
+    GameElemTranslate[1] = 20 * (yCount / 2) - (20 * y)
+    E.game.style.transform = `translate(${GameElemTranslate[0]}px, ${GameElemTranslate[1]}px)`
+    E_WORLD[y][x].setAttribute("class", "player")
+}
 
-document.addEventListener('keydown', (e) => {
+//#region 이벤트 리스너
+document.body.addEventListener('keydown', (e) => {
     switch (e.key) {
         case "ArrowUp":
         case "ArrowRight":
@@ -62,41 +76,49 @@ document.addEventListener('keydown', (e) => {
             E.HUD.devInfo.main.classList.toggle("invisibility")
             e.preventDefault()
             break;
+        case "/":
+            E.HUD.devConsole.main.classList.toggle("invisibility")
+            E.HUD.devConsole.input.focus()
+            e.preventDefault()
+            break;
     }
 })
-
+E.HUD.devConsole.input.addEventListener("keydown", (e) => {
+    console.log(e.key)
+    if (e.key == "Enter") {
+        const v = E.HUD.devConsole.input.value
+        if (/^tp \d+ \d+$/gm.test(v)) {
+            const vA = v.split(" ")
+            renderPlayer(Player[0], Player[1], parseInt(vA[1]), parseInt(vA[2]))
+        }
+        else return;
+        E.HUD.devConsole.input.value = ""
+    }
+    else if (e.key == "Escape") 
+        E.HUD.devConsole.main.classList.toggle("invisibility")
+    e.stopImmediatePropagation()
+})
+//#endregion
 function PlayerMOVINGMAN(key) {
-    const oldPlayer = {...Player};
-    let canMovePlayer = true;
+    const myPlayer = {...Player};
     switch (key) {
         case "ArrowUp":
             if (Player[1] === 0) return canMovePlayer = false;
-            Player[1]--
-            GameElemTranslate[1] += 20
+            myPlayer[1]--
             break;
         case "ArrowRight":
             if (Player[0] === WORLD[0].length - 1) return canMovePlayer = false;
-            Player[0]++
-            GameElemTranslate[0] -= 20
+            myPlayer[0]++
             break;
         case "ArrowDown":
             if (Player[1] === WORLD.length - 1) return canMovePlayer = false;
-            Player[1]++
-            GameElemTranslate[1] -= 20
+            myPlayer[1]++
             break;
         case "ArrowLeft":
             if (Player[0] === 0) return canMovePlayer = false;
-            Player[0]--
-            GameElemTranslate[0] += 20
+            myPlayer[0]--
             break;
     }
-    if (canMovePlayer)
-        E_WORLD[oldPlayer[1]][oldPlayer[0]].setAttribute("class", WORLD[oldPlayer[1]][oldPlayer[0]]) // 플레이어가 있던 곳을 원래 데이터로 변경
-    // if (Player[0] > Math.ceil(xCount / 2))
-    //     GameElemTranslate[0] -= 20
-    // if (Player[1] > Math.ceil(yCount / 2))
-    //     GameElemTranslate[1] -= 20
-    E.game.style.transform = `translate(${GameElemTranslate[0]}px, ${GameElemTranslate[1]}px)`
-    E_WORLD[Player[1]][Player[0]].setAttribute("class", "player")
+    renderPlayer(Player[0], Player[1], myPlayer[0], myPlayer[1])
     renderHUD()
 }
