00bd950376b39ab6a0e34fac095b48de240ed892
museekee <103117239+museekee@users.noreply.github.com>
2023-03-01 17:19:32 +0900

    속도변경기능

--- web/assets/scripts/land.js
+++ web/assets/scripts/land.js
@@ -15,7 +15,6 @@ const E = {
     }
 }
 const BLOCK_SIZE = 50
-let Selected_Block = "grass";
 const yCount = Math.ceil(window.innerHeight / BLOCK_SIZE)
 const xCount = Math.ceil(window.innerWidth / BLOCK_SIZE)
 const GameElemTranslate = [BLOCK_SIZE * (xCount / 2), BLOCK_SIZE * (yCount / 2)]
@@ -24,7 +23,11 @@ const GameElemTranslate = [BLOCK_SIZE * (xCount / 2), BLOCK_SIZE * (yCount / 2)]
  * @type {string[][]}
  */
 const WORLD = localStorage.getItem("key") ?? JSON.parse(JSON.stringify(new Array(250).fill(new Array(250).fill("grass"))))
-const Player = [0, 0]
+const Player = {
+    position: [0, 0],
+    block: "grass",
+    speed: 1,
+}
 /**
  * @type {HTMLDivElement[][]}
  */
@@ -46,8 +49,8 @@ renderHUD()
 //#endregion
 
 function renderHUD() {
-    E.HUD.devInfo.posX.innerText = `x : ${Player[0]}`
-    E.HUD.devInfo.posY.innerText = `y : ${Player[1]}`
+    E.HUD.devInfo.posX.innerText = `x : ${Player.position[0]}`
+    E.HUD.devInfo.posY.innerText = `y : ${Player.position[1]}`
 }
 
 function renderByPos(x, y, data) {
@@ -55,8 +58,8 @@ function renderByPos(x, y, data) {
     E_WORLD[y][x].setAttribute("class", data)
 }
 function renderPlayer(x, y, degree) {
-    Player[0] = x
-    Player[1] = y
+    Player.position[0] = x
+    Player.position[1] = y
     GameElemTranslate[0] = BLOCK_SIZE * (xCount / 2) - (BLOCK_SIZE * x)
     GameElemTranslate[1] = BLOCK_SIZE * (yCount / 2) - (BLOCK_SIZE * y)
     E.game.style.transform = `translate(${GameElemTranslate[0]}px, ${GameElemTranslate[1]}px)`
@@ -93,16 +96,22 @@ E.HUD.devConsole.main.addEventListener("keydown", (e) => {
         const vA = v.split(" ")
         if (v == "help") {
             devLog(`=====HELP=====
-                tp [x] [y] [degree?] : Player을(를) [x] [y] 위치로 이동하고 [degree]각도로 변경`)
+                tp [x|~] [y|~] [degree?] : Player을(를) [x] [y] 위치로 이동하고 [degree]각도로 변경
+                setspeed [speed|*] Player을(를) [speed]속도로 변경`)
         }
         else if (/^tp (\d+|~) (\d+|~)( \d+(\.\d+)?)?$/gm.test(v)) {
             // TP
             renderPlayer(
-                vA[1] == "~" ? Player[0] : parseInt(vA[1]),
-                vA[2] == "~" ? Player[1] : parseInt(vA[2]),
+                vA[1] == "~" ? Player.position[0] : parseInt(vA[1]),
+                vA[2] == "~" ? Player.position[1] : parseInt(vA[2]),
                 parseFloat(vA[3] ?? "0")
             )
-            devSuccess(`Player을(를) (${vA[1].replace("~", Player[0])}, ${vA[2].replace("~", Player[1])})로 이동하고 각도를 ${vA[3] ?? "0"}º로 바꾸는 것`)
+            devSuccess(`Player을(를) (${vA[1].replace("~", Player.position[0])}, ${vA[2].replace("~", Player.position[1])})로 이동하고 각도를 ${vA[3] ?? "0"}º로 바꾸는 것`)
+        }
+        else if (/^setspeed (\d+|\*)$/gm.test(v)) {
+            // SetSpeed
+            Player.speed = parseInt(vA[1].replace("*", "1"))
+            devSuccess(`Player의 속도를 '${vA[1].replace("*", "1")}'(으)로 바꾸는 것`)
         }
         else return devError(`'${vA[0]}'은(는) 존재하는 명령어가 아니거나, 명령어가 올바른지 검증한 다음 다시 시도 하십시오.`);
         E.HUD.devConsole.input.value = ""
@@ -113,10 +122,10 @@ E.HUD.devConsole.main.addEventListener("keydown", (e) => {
 })
 for (const item of document.getElementById("hotbar").getElementsByClassName("item")) {
     item.addEventListener("mouseup", (e) => {
-        Selected_Block = e.target.dataset.id
+        Player.block = e.target.dataset.id
         console.log(e.target.classList)
         document.querySelectorAll("#hotbar .item").forEach(elem => {
-            if (elem.dataset.id === Selected_Block)
+            if (elem.dataset.id === Player.block)
                 elem.classList.add("selected")
             else
                 elem.classList.remove("selected")
@@ -125,68 +134,72 @@ for (const item of document.getElementById("hotbar").getElementsByClassName("ite
 }
 //#endregion
 function PlayerMOVINGMAN(e) {
-    const myPlayer = {...Player};
+    const myPlayer = {...Player.position};
     let degree = 0
     let canMovePlayer = true
     console.log(e)
+    const isOverTop = Player.position[1] <= 0
+    const isOverRight = Player.position[0] >= WORLD[0].length - 1
+    const isOverBottom = Player.position[1] >= WORLD.length - 1
+    const isOverLeft = Player.position[0] <= 0
     switch (e.key) {
         case "ArrowUp":
             degree = 0
-            if (Player[1] === 0) {
+            if (isOverTop) {
                 canMovePlayer = false;
                 break;
             }
-            myPlayer[1]--
+            myPlayer[1] -= 1 * Player.speed
             break;
         case "ArrowRight":
             degree = 90
-            if (Player[0] === WORLD[0].length - 1) {
+            if (isOverRight) {
                 canMovePlayer = false;
                 break;
             }
             if (e.ctrlKey) {
                 degree = 45
-                myPlayer[0]++
-                if (Player[1] !== 0) myPlayer[1]--
+                myPlayer[0] += 1 * Player.speed
+                if (!isOverTop) myPlayer[1] -= 1 * Player.speed
             }
             else if (e.altKey) {
                 degree = 135
-                myPlayer[0]++
-                if (Player[1] !== WORLD.length - 1) myPlayer[1]++
+                myPlayer[0] += 1 * Player.speed
+                if (!isOverBottom) myPlayer[1] += 1 * Player.speed
             }
-            else myPlayer[0]++
+            else myPlayer[0] += 1 * Player.speed
             break;
         case "ArrowDown":
             degree = 180
-            if (Player[1] === WORLD.length - 1) {
+            if (isOverBottom) {
                 canMovePlayer = false;
                 break;
             }
-            myPlayer[1]++
+            myPlayer[1] += 1 * Player.speed
             break;
         case "ArrowLeft":
             degree = 270
-            if (Player[0] === 0) {
+            if (isOverLeft) {
                 canMovePlayer = false;
                 break;
             }
             if (e.ctrlKey) {
                 degree = 315
-                myPlayer[0]--
-                if (Player[1] !== 0) myPlayer[1]--
+                myPlayer[0] -= 1 * Player.speed
+                if (!isOverTop)  myPlayer[1] -= 1 * Player.speed
             }
             else if (e.altKey) {
                 degree = 225
-                myPlayer[0]--
-                if (Player[1] !== WORLD.length - 1) myPlayer[1]++
+                myPlayer[0] -= 1 * Player.speed
+                if (!isOverBottom)  myPlayer[1] += 1 * Player.speed
             }
-            else myPlayer[0]--
+            else myPlayer[0] -= 1 * Player.speed
             break;
     }
     if (canMovePlayer)
         renderPlayer(myPlayer[0], myPlayer[1], degree)
     else
-        renderPlayer(Player[0], Player[1], degree)
+        renderPlayer(Player.position[0], Player.position[1], degree)
     renderHUD()
 }
 
@@ -194,7 +207,7 @@ function devError(text) {
     callDevLog(text, "#ff0000")
 }
 function devSuccess(text) {
-    callDevLog(`${text}을(를) 성공적으로 수행하였습니다.`, "#00ff00")
+    callDevLog(`${text}을 성공적으로 수행하였습니다.`, "#00ff00")
 }
 function devLog(text) {
     callDevLog(text, "#eeeeee")
